4bf7ef4344d135560f3c6b46bcce7ee9d7433d45
Pong <pongpong34756@gmail.com>
2024-07-24 14:39:23 +0700

    bubble sort algo hw

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,24 @@
 function sortStudentsByScore(students) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let n = students.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (students[j].score < students[j + 1].score) {
+        let temp = students[j];
+        students[j] = students[j + 1];
+        students[j + 1] = temp;
+      }
+    }
+  }
+  return students;
 }
+
+const students = [
+  { id: "S001", name: "Anan", score: 85 },
+  { id: "S002", name: "Boon", score: 92 },
+  { id: "S003", name: "Chai", score: 78 },
+  { id: "S004", name: "Dao", score: 95 },
+  { id: "S005", name: "Ek", score: 88 },
+  { id: "S006", name: "Fah", score: 80 },
+];
+
+console.log(sortStudentsByScore(students));
